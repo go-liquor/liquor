d1120be0f3758905f1b421059f563c72d7575913
Lucas Bernardo <lbernardo.brito@gmail.com>
2025-01-29 19:07:52 -0300

    chore: first commit :rocket:

---
+++ sdk/app/app.go
@@ -0,0 +1,25 @@
+package app
+
+import (
+	"github.com/go-liquor/liquor/sdk/config"
+	"github.com/go-liquor/liquor/sdk/logger"
+	"go.uber.org/fx"
+)
+
+// NewApp create a new app
+func NewApp(modules ...fx.Option) {
+	options := []fx.Option{
+		config.ConfigModule,
+		logger.LoggerModule,
+	}
+	options = append(options, modules...)
+	app := fx.New(options...)
+	app.Run()
+}
+
+// RegisterService register you services
+func RegisterService(services ...interface{}) fx.Option {
+	return fx.Module("liquor-app-services", fx.Provide(
+		services...,
+	))
+}
