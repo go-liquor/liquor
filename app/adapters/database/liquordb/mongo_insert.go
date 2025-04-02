a4dbfd1ebdd8aba763e3eb412fa6a4b98c02a93a
Lucas Bernardo <lbernardo.brito@gmail.com>
2025-04-02 11:11:26 -0300

    :feat: add insertMany when have slice

--- app/adapters/database/liquordb/mongo_insert.go
+++ app/adapters/database/liquordb/mongo_insert.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"reflect"
 	"time"
 )
 
@@ -19,19 +20,33 @@ func (i *MongoInsertBase) Exec(ctx context.Context) (any, error) {
 	if i.err != nil {
 		return "", i.err
 	}
-	content, err := bson.Marshal(i.collection)
-	if err != nil {
-		return "", fmt.Errorf("failed to marshal collection content: %w", err)
+	t := reflect.TypeOf(i.collection)
+	if t.Kind() == reflect.Ptr {
+		content, err := bson.Marshal(i.collection)
+		if err != nil {
+			return "", fmt.Errorf("failed to marshal collection content: %w", err)
+		}
+		data := bson.M{}
+		if err := bson.Unmarshal(content, &data); err != nil {
+			return "", fmt.Errorf("failed to unmarshal collection content: %w", err)
+		}
+		data["createdAt"] = time.Now()
+		data["updatedAt"] = time.Now()
+		res, err := i.db.Collection(i.collectionName).InsertOne(ctx, data)
+		if err != nil {
+			return "", err
+		}
+		return res.InsertedID, err
 	}
-	data := bson.M{}
-	if err := bson.Unmarshal(content, &data); err != nil {
-		return "", fmt.Errorf("failed to unmarshal collection content: %w", err)
-	}
-	data["createdAt"] = time.Now()
-	data["updatedAt"] = time.Now()
-	res, err := i.db.Collection(i.collectionName).InsertOne(ctx, data)
+	res, err := i.db.Collection(i.collectionName).InsertMany(ctx, i.collection)
 	if err != nil {
 		return "", err
 	}
-	return res.InsertedID, err
+	now := time.Now()
+	i.db.Collection(i.collectionName).UpdateMany(ctx, bson.M{"_id": bson.M{"$in": res.InsertedIDs}}, bson.M{"$set": bson.M{
+		"createdAt": now,
+		"updatedAt": now,
+	}})
+	return "", nil
+
 }
