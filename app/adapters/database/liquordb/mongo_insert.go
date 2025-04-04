16508c9bf1d53dfb6045a900585d9e1576f0a6ae
Lucas Bernardo <lbernardo.brito@gmail.com>
2025-04-03 22:18:32 -0300

    fix: the InsertMany have problems

--- app/adapters/database/liquordb/mongo_insert.go
+++ app/adapters/database/liquordb/mongo_insert.go
@@ -22,31 +22,35 @@ func (i *MongoInsertBase) Exec(ctx context.Context) (any, error) {
 	}
 	t := reflect.TypeOf(i.collection)
 	if t.Kind() == reflect.Ptr {
-		content, err := bson.Marshal(i.collection)
-		if err != nil {
-			return "", fmt.Errorf("failed to marshal collection content: %w", err)
+		if t.Elem().Name() != "" {
+			content, err := bson.Marshal(i.collection)
+			if err != nil {
+				return "", fmt.Errorf("failed to marshal collection content: %w", err)
+			}
+			data := bson.M{}
+			if err := bson.Unmarshal(content, &data); err != nil {
+				return "", fmt.Errorf("failed to unmarshal collection content: %w", err)
+			}
+			data["createdAt"] = time.Now()
+			data["updatedAt"] = time.Now()
+			res, err := i.db.Collection(i.collectionName).InsertOne(ctx, data)
+			if err != nil {
+				return "", err
+			}
+			return res.InsertedID, err
 		}
-		data := bson.M{}
-		if err := bson.Unmarshal(content, &data); err != nil {
-			return "", fmt.Errorf("failed to unmarshal collection content: %w", err)
-		}
-		data["createdAt"] = time.Now()
-		data["updatedAt"] = time.Now()
-		res, err := i.db.Collection(i.collectionName).InsertOne(ctx, data)
+		res, err := i.db.Collection(i.collectionName).InsertMany(ctx, i.collection)
 		if err != nil {
 			return "", err
 		}
-		return res.InsertedID, err
+		now := time.Now()
+		i.db.Collection(i.collectionName).UpdateMany(ctx, bson.M{"_id": bson.M{"$in": res.InsertedIDs}}, bson.M{"$set": bson.M{
+			"createdAt": now,
+			"updatedAt": now,
+		}})
+		return "", nil
 	}
-	res, err := i.db.Collection(i.collectionName).InsertMany(ctx, i.collection)
-	if err != nil {
-		return "", err
-	}
-	now := time.Now()
-	i.db.Collection(i.collectionName).UpdateMany(ctx, bson.M{"_id": bson.M{"$in": res.InsertedIDs}}, bson.M{"$set": bson.M{
-		"createdAt": now,
-		"updatedAt": now,
-	}})
-	return "", nil
+
+	return "", fmt.Errorf("failed to read as pointer or slice")
 
 }
